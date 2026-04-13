c4be9fbf7c3b8d8ffe2e7fee1265cb6a93765b32
kreir42 <gortle97@gmail.com>
2026-04-13 18:34:44 +0200

    more efficient image function

--- image.h
+++ image.h
@@ -1,37 +1,40 @@
 #include <sys/stat.h>
 #include <time.h>
 
+static inline void draw_image(struct rule* rule, const char* path) {
+	struct ncvisual* visual = ncvisual_from_file(path);
+	if (visual) {
+		struct ncvisual_options vopts = {
+			.n = rule->window,
+			.scaling = NCSCALE_SCALE,
+			.y = NCALIGN_CENTER,
+			.x = NCALIGN_CENTER,
+			.blitter = NCBLIT_DEFAULT,
+			.flags = NCVISUAL_OPTION_HORALIGNED | NCVISUAL_OPTION_VERALIGNED,
+		};
+		ncvisual_blit(nc, visual, &vopts);
+		ncvisual_destroy(visual);
+		stage_refresh(rule);
+	}
+}
+
 void* image(void* input){
 	struct rule* rule = input;
 	char* path = rule->data;
 	int t = rule->time;
-	time_t last_mtime = 0;
 
-	while(1){
-		struct stat st;
-		if (stat(path, &st) == 0) {
-			if (st.st_mtime > last_mtime) {
+	if (t <= 0) {
+		draw_image(rule, path);
+	} else {
+		time_t last_mtime = 0;
+		while(1){
+			struct stat st;
+			if(stat(path, &st) == 0 && st.st_mtime > last_mtime){
 				last_mtime = st.st_mtime;
-				struct ncvisual* visual = ncvisual_from_file(path);
-				if (visual) {
-					struct ncvisual_options vopts = {
-						.n = rule->window,
-						.scaling = NCSCALE_SCALE,
-						.y = NCALIGN_CENTER,
-						.x = NCALIGN_CENTER,
-						.blitter = NCBLIT_DEFAULT,
-						.flags = NCVISUAL_OPTION_HORALIGNED | NCVISUAL_OPTION_VERALIGNED,
-					};
-					ncvisual_blit(nc, visual, &vopts);
-					ncvisual_destroy(visual);
-					stage_refresh(rule);
-				}
+				draw_image(rule, path);
 			}
+			sleep(t);
 		}
-		if (t <= 0) {
-			break;
-		}
-		sleep(t);
 	}
 	return NULL;
 }
