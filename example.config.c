99cf41f751358fcd3ac300600764af787328984f
kreir42 <gortle97@gmail.com>
2026-04-13 18:07:59 +0200

    skip unneeded image redraws by tracking file modification time

--- example.config.c
+++ example.config.c
@@ -9,8 +9,8 @@ static struct rule rules[] = {
 //	function             y     x      h      w   time (s) NULL  flags                                       argument
 	image,               0,    0,     1,     1,     1000, NULL, CENTER|RELATIVE_SIZE|OPAQUE,                "example_background.png",
 	print_string,        0,    0,     1,    34,        0, NULL, CENTER_X,                                   "EXAMPLE WITH WIDE CHARACTERS: 漢字",
-	timedate,          0.2,    0,     1,    23,        1, NULL, CENTER_X|RELATIVE_Y_POS|BLEND_BACKGROUND,   "%Y-%m-%d %a %H:%M:%S",
-	external_command,    0,    0,     8,    42,   6*3600, NULL, CENTER|DRAW_BOX,                            "cal -m -n 2 --color=always",
+	timedate,          0.2,    0,     1,    23,        1, NULL, CENTER_X|RELATIVE_Y_POS,                    "%Y-%m-%d %a %H:%M:%S",
+	external_command,    0,    0,     8,    42,   6*3600, NULL, CENTER|DRAW_BOX|BLEND_BACKGROUND,           "cal -m -n 2 --color=always",
 	print_string,        0,    0,     1,     9,        0, NULL, 0,                                          "CPU usage",
 	plot,                0,    0,    20,    40,        1, NULL, DRAW_BOX,                                   "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{print 100 - $1}'",
 };
