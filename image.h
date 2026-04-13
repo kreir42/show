16656dfbff78952de8456bb8015d75fc5acd7eed
kreir42 <gortle97@gmail.com>
2026-04-13 17:00:41 +0200

    forgot the image header file

---
+++ image.h
@@ -0,0 +1,27 @@
+void* image(void* input){
+	struct rule* rule = input;
+	char* path = rule->data;
+	int t = rule->time;
+
+	while(1){
+		struct ncvisual* visual = ncvisual_from_file(path);
+		if (visual) {
+			struct ncvisual_options vopts = {
+				.n = rule->window,
+				.scaling = NCSCALE_SCALE,
+				.y = NCALIGN_CENTER,
+				.x = NCALIGN_CENTER,
+				.blitter = NCBLIT_DEFAULT,
+				.flags = NCVISUAL_OPTION_HORALIGNED | NCVISUAL_OPTION_VERALIGNED,
+			};
+			ncvisual_blit(nc, visual, &vopts);
+			ncvisual_destroy(visual);
+			stage_refresh(rule);
+		}
+		if (t <= 0) {
+			break;
+		}
+		sleep(t);
+	}
+	return NULL;
+}
