e621700bfd8ccf995d6397d2b522715756d45c58
kreir42 <gortle97@gmail.com>
2026-06-18 14:30:48 +0200

    fixed wrong size and undetermined behavior on overflow for timedate command

--- include.h
+++ include.h
@@ -152,9 +152,9 @@ void* timedate(void* input){
 	struct rule* rule = input;
 	int h, w;
 	get_size(rule, &h, &w);
-	w++;	//+1 for the NULL terminator
+	size_t size = w*MB_CUR_MAX + 1; //w columns, up to MB_CUR_MAX bytes each for multi-byte output, +1 for the NULL terminator
 
-	char* str = malloc(w*sizeof(char));
+	char* str = malloc(size);
 	if(!str) return NULL; //check for failed malloc
 	pthread_cleanup_push(free, str); //free str on thread cancel
 
@@ -163,7 +163,8 @@ void* timedate(void* input){
 	while(1){
 		t = time(NULL);
 		tm = localtime(&t);
-		strftime(str, w, rule->data, tm);
+		strftime(str, size, rule->data, tm);
+		str[size-1] = '\0'; //on overflow, strftime returns 0 and buffer is undefined, so add NULL terminator just in case
 		draw_string(rule, 0, 0, str);
 		stage_refresh(rule);
 		sleep(rule->time);
